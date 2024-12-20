bda6297c35ffea21ab8b1a4f7a19a3e071f96ce7
Tanida <printer.rung@gmail.com>
2024-12-20 21:15:37 +0700

    lab10_1 completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,27 +1,44 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
-int main(){
-	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
-	do{
-		cout << "Student [" << "]: ";
-		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
-			//Do something
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
-		} 
-	}while(true);
-	
-	
-	cout << "In total ? students.";
-	cout << "A = " << count[0] <<", ";
-	cout << "B = " << count[1] <<", ";	
-	//	and so on ... for grade = C, D, F	
-	
-	return 0;
+int main() {
+    int count[5] = {0}; // นับจำนวนนักเรียนแต่ละเกรด
+    int totalStudents = 0; // นับจำนวนนักเรียนทั้งหมด
+    string grade;
+
+    cout << "Please input grade of each student (A-F) or input 0 to exit." << endl;
+
+    int studentNum = 1;
+    while (true) {
+        cout << "Student [" << studentNum << "]: ";
+        cin >> grade;
+
+        if (grade == "0") {
+            break; // ออกจากลูปเมื่อใส่ 0
+        } else if (grade == "A") {
+            count[0]++;
+        } else if (grade == "B") {
+            count[1]++;
+        } else if (grade == "C") {
+            count[2]++;
+        } else if (grade == "D") {
+            count[3]++;
+        } else if (grade == "F") {
+            count[4]++;
+        } else {
+            cout << "Wrong input. Please input again." << endl;
+            studentNum--;
+            totalStudents--;// ไม่นับเป็นนักเรียนหนึ่งคน
+        }
+
+        totalStudents++;
+        studentNum++;
+    }
+
+    cout << "In total " << totalStudents << " students." << endl;
+    cout << "A = " << count[0] << ", B = " << count[1] << ", C = " << count[2] << ", D = " << count[3] << ", F = " << count[4] << endl;
+
+    return 0;
 }
