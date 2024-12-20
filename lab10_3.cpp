9cef0fc3b9c4cb42d191d1c48952f31bf739c6ae
Tanida <printer.rung@gmail.com>
2024-12-20 21:35:57 +0700

    lab10_3 completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,35 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int count = 0;
+    double sum = 0.0;
+    double sum_of_squares = 0.0;
+    string textline;
+    ifstream source("score.txt");
+
+    while (getline(source, textline)) {
+        double number = stof(textline); // Convert string to float
+        sum += number;
+        sum_of_squares += number * number;
+        count++;
+    }
+
+    source.close();
+
+    double mean = sum / count;
+    double variance = (sum_of_squares / count) - (mean * mean);
+    double standard_deviation = sqrt(variance);
+
+    cout << "Number of data = " << count << endl;
+    cout << fixed << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << standard_deviation << endl;
+
+    return 0;
+}
