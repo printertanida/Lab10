d376951b31d86f652f9a14da207a855eb6d52b41
Tanida <printer.rung@gmail.com>
2024-12-20 21:19:21 +0700

    lab10_2 completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -8,6 +8,13 @@ int main (){
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
+
+		dest << "-------------------- BOOM ---------------------" << endl;
+    string line;
+    while (getline(source, line)) {
+        dest << line << endl;
+    }
+    dest << "-------------------- HA!! ---------------------" << endl;
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
